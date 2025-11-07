e70c2a0c9839af7aa6b77179b15d5acaae76546b
Dmytro Kovalchuk <dmytrokovalchuk.sl@gmail.com>
2025-11-07 10:22:56 +0200

    Create get-citizens.js

---
+++ netlify/functions/get-citizens.js
@@ -0,0 +1,22 @@
+//netlify/functions/get-citizens.js
+exports.handler = async function() {
+    const response = await fetch(
+        `${process.env.NOCODB_URL}${process.env.TABLE_ID}/records`,
+        {
+            headers: {
+                'xc-token': process.env.NOCODB_TOKEN
+            }
+        }
+    );
+    
+    const data = await response.json();
+    
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+};
